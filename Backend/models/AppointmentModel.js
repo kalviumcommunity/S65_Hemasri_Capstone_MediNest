c1dc0fd87d64b10ed3b7954334f211a6846a160f
Hemasri1709 <guthulahemasri410@gmail.com>
2025-04-14 09:59:59 +0530

    Added mongoose models for Doctor, Patient and Appointment

---
+++ Backend/models/AppointmentModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const appointmentSchema = new mongoose.Schema({
+    patientId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Patient',
+        required: true
+    },
+    doctorId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Doctor',
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Appointment', appointmentSchema);
---
+++ Backend/models/PatientModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const patientSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Patient', patientSchema);
